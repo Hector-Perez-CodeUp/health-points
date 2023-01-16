1fcd8893dc7d0d870a4b6fa6da2f74385a2bde59
Hector Perez <hector.elliott.perez@gmail.com>
2023-01-15 18:13:04 -0800

    added stopwatch to just-breathe app

--- js/just-breathe-script.js
+++ js/just-breathe-script.js
@@ -1,17 +1,44 @@
-let numberDiv = document.getElementById("numberDiv");
-let number = 0;
+var seconds = 0;
+var tens = 0;
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+var buttonStart = document.getElementById("button-start");
+var buttonStop = document.getElementById('button-stop')
+var buttonReset = document.getElementById("button-reset");
+var interval;
 
-function breathingTimer() {
-    while (number <= 4) {
-        setTimeout(function () {
-            numberDiv.innerHTML = ('<p>${number}</p>' + number);
-            number = number + 1;
-        }, 1000);
+function startTimer() {
+    tens++;
+    if (tens < 9) {
+        appendTens.innerHTML = "0" + tens;
+    }
+    else if (tens > 9) {
+        appendTens.innerHTML = tens;
+    }
+    else if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + 0;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+    else if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
     }
 }
 
-breathingTimer();
+buttonStart.onclick = function() {
+    interval = setInterval(startTimer);
+}
 
-setTimeout(function(){
-    console.log("Hello World");
-}, 5000);
+buttonStop.onclick = function() {
+    clearInterval(interval);
+}
+
+buttonReset.onclick = function() {
+    clearInterval(interval);
+    tens = "0";
+    seconds = "0";
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
+
+}
